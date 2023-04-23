c39cface30a3e1a2703e8ac29f843bd7fe439e54
coolyeekk <97148614+coolyeekk@users.noreply.github.com>
2023-04-23 17:20:37 +0800

    Update newcovid.py

--- newcovid.py
+++ newcovid.py
@@ -24,9 +24,6 @@ st.markdown("<h4 style='text-align: justify; color: blue;'>This dashboard is an 
 
 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-st.markdown("<h3 style='text-align: center;'>New Cases</h3>",
-            unsafe_allow_html=True)
-num1 = df['cases_new'][0]
 st.markdown(
     f"<h2 style='text-align: center; color: blue;'>{num1}</h2>", unsafe_allow_html=True)
 
