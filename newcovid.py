ddc1d068999d93402d9ef8e3db02c70823cb5be0
coolyeekk <97148614+coolyeekk@users.noreply.github.com>
2023-04-23 17:33:47 +0800

    Update newcovid.py

--- newcovid.py
+++ newcovid.py
@@ -73,14 +73,16 @@ st.markdown("<h2 style='text-align: center;'>Visualizing Total and Daily Cases</
 first_chart, second_chart = st.beta_columns(2)
 
 with first_chart:
-    fig = px.line(df1, x="date_last_onset", y=["cases_total",
-                                    "cases_new", "cases_active"], title="Total Confirmed, new and active")
+    fig = px.line(df1, x=pd.to_datetime(df1["date_announced"]).dt.strftime('%b %d %Y'), 
+              y=[ "cases_new", "tests"], title="Total New Case vs Test out Positivity Rate")
+
     fig.update_layout(height=600)
     st.plotly_chart(fig, use_container_width=True)
 
 with second_chart:
-    fig = px.line(df1, x="date_last_onset", y=["Daily Confirmed",
-                                    "Daily Deceased", "Daily Recovered"], title="Daily Confirmed, Recovered and Deceased")
+    fig = px.line(df1, x=pd.to_datetime(df1["date_announced"]).dt.strftime('%b %d %Y'),
+              y=["icu", "deaths"], title="Total in ICU and Deaths")
+
     fig.update_layout(height=600)
     st.plotly_chart(fig, use_container_width=True)
 
