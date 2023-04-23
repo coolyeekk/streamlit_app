53a0ba783bf6e6bdee89c5b7e77dfc0b07051be2
coolyeekk <97148614+coolyeekk@users.noreply.github.com>
2023-04-23 17:35:44 +0800

    Update newcovid.py

--- newcovid.py
+++ newcovid.py
@@ -67,21 +67,23 @@ st.markdown("---")
 df1 = pd.read_csv(
     "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/clusters.csv")
 
-st.markdown("<h2 style='text-align: center;'>Visualizing Total and Daily Cases</h2>",
+st.markdown("<h2 style='text-align: center;'>Visualizing Total Cases & ICU & Deaths</h2>",
             unsafe_allow_html=True)
 
 first_chart, second_chart = st.beta_columns(2)
 
 with first_chart:
     fig = px.line(df1, x=pd.to_datetime(df1["date_announced"]).dt.strftime('%b %d %Y'), 
-              y=[ "cases_new", "tests"], title="Total New Case vs Test out Positivity Rate")
+              y=[ "cases_new", "tests"], title="Total New Case vs Test out Positivity Rate",
+                 line_color=["blue", "green"]))
 
     fig.update_layout(height=600)
     st.plotly_chart(fig, use_container_width=True)
 
 with second_chart:
     fig = px.line(df1, x=pd.to_datetime(df1["date_announced"]).dt.strftime('%b %d %Y'),
-              y=["icu", "deaths"], title="Total in ICU and Deaths")
+              y=["icu", "deaths"], title="Total in ICU and Deaths",
+                 line_color=["blue", "green"]))
 
     fig.update_layout(height=600)
     st.plotly_chart(fig, use_container_width=True)
