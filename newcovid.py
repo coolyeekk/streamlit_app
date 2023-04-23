df21c3cef9ea15cea2fef821f513e9cc21ef816c
coolyeekk <97148614+coolyeekk@users.noreply.github.com>
2023-04-23 17:51:39 +0800

    Update newcovid.py

--- newcovid.py
+++ newcovid.py
@@ -74,7 +74,7 @@ first_chart, second_chart = st.beta_columns(2)
 
 with first_chart:
     fig = px.line(df1, x=pd.to_datetime(df1["date_announced"]).dt.strftime('%b %d %Y'), 
-              y=["cases_new", "tests"], 
+              y=["cases_active", "recovered"], 
               title="Total New Case vs Test out Positivity Rate",
               color_discrete_sequence=["blue", "green"])
 
@@ -82,8 +82,8 @@ with first_chart:
     st.plotly_chart(fig, use_container_width=True)
 
 with second_chart:
-    fig = px.line(df1, x=pd.to_datetime(df1["date_announced"]).dt.strftime('%b %d %Y'),
-              y=["icu", "deaths"],
+    fig = px.line(df1, x=df1["district"],
+              y=["tests", "cases_total"],
               title="Total in ICU and Deaths",
               color_discrete_sequence=["blue", "green"])
 
