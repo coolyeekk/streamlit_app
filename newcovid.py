d4c1bf6794bf2e04479526debf5513b1b315ccb7
coolyeekk <97148614+coolyeekk@users.noreply.github.com>
2023-04-24 16:54:19 +0800

    Update newcovid.py

--- newcovid.py
+++ newcovid.py
@@ -120,7 +120,8 @@ fig = go.Figure(data=[active_cases, new_cases, recovered_cases])
 fig.update_layout(title=f"COVID-19 Cases in {state_selected}", xaxis_title="Date", yaxis_title="Number of Cases")
 
 # Display the plot
-st.plotly_chart(fig, layout={'width': 600, 'height': 600})
+fig.update_layout(height=800)
+st.plotly_chart(fig, use_container_width=True)
 
 
 
